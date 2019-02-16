9df07302d3497d169ee329ddc65d57963266e11f
Alexander <37543796+Alexander88207@users.noreply.github.com>
2019-02-16 15:34:08 +0100

    put all the script's in one :)

--- Installer.sh
+++ Installer.sh
@@ -4,7 +4,7 @@ WIDTH=40
 CHOICE_HEIGHT=4
 BACKTITLE="RTL8812au-Installer"
 TITLE="Distro Selector"
-MENU="Choose one of the following options:"
+MENU="Choose one of the following distro's:"
 
 OPTIONS=(1 "Arch"
          2 "Solus"
@@ -23,23 +23,186 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         1)
-			cd shellscripts/
-			bash rtl8812au-arch.sh
+			#!/bin/bash
+echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
+sudo pacman -S bc dkms git linux-headers
+echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
+git clone https://aur.archlinux.org/rtl8812au-dkms-git.git
+cd rtl8812au-dkms-git
+echo -e "\e[40;38;5;82m Driver will be built & installed \e[30;48;5;82m\e[0m"
+makepkg -i
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
             ;;
         2)
-			cd shellscripts/
-			bash rtl8812au-solus.sh
+echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
+sudo eopkg it -c system.devel
+sudo eopkg it linux-current-headers
+sudo eopkg it libelf-devel
+echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
+git clone https://github.com/gordboy/rtl8812au.git
+cd rtl8812au
+echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
+make
+cho -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
+sudo modprobe 8812au
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
+
+
             ;;
         3)
-                        cd shellscripts/
-			bash rtl8812au-ubuntu.sh
+                        #!/bin/bash
+sudo apt update
+echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
+sudo apt install -y git build-essential make autoconf libtool gcc gettext
+echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
+git clone https://github.com/gordboy/rtl8812au
+cd rtl8812au
+echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
+make
+echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
+sudo modprobe 8812au
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
+
+
             ;;
         4)
-			cd shellscripts/
-			bash rtl8812au-sabayon.sh
+			#/bin/bash
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=6
+BACKTITLE="RTL8812au-Installer"
+TITLE="Sabayon sources installation"
+MENU="Choose your kernel version of sabayon:"
+
+OPTIONS=(1 "4.4"
+         2 "4.9"
+         3 "4.14"
+         4 "4.18"
+	 5 "4.19"
+	 6 "4.20")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+			#!/bin/bash
+echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
+sudo equo install gcc --ask
+sudo equo i sys-kernel/sabayon-sources:4.4 --ask
+echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
+git clone https://github.com/gordboy/rtl8812au.git
+cd rtl8812au
+echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
+make
+echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
+sudo modprobe 8812au
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
+
+            ;;
+        2)
+			#!/bin/bash
+echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
+sudo equo install gcc --ask
+sudo equo i sys-kernel/sabayon-sources:4.9 --ask
+echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
+git clone https://github.com/gordboy/rtl8812au.git
+cd rtl8812au
+echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
+make
+echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
+sudo modprobe 8812au
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
+
+            ;;
+        3)
+                        #!/bin/bash
+echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
+sudo equo install gcc --ask
+sudo equo i sys-kernel/sabayon-sources:4.14 --ask
+echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
+git clone https://github.com/gordboy/rtl8812au.git
+cd rtl8812au
+echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
+make
+echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
+sudo modprobe 8812au
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
+
+            ;;
+        4)
+                        #!/bin/bash
+echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
+sudo equo install gcc --ask
+sudo equo i sys-kernel/sabayon-sources:4.18 --ask
+echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
+git clone https://github.com/gordboy/rtl8812au.git
+cd rtl8812au
+echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
+make
+echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
+sudo modprobe 8812au
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
+
+            ;;
+	5)
+                        #!/bin/bash
+echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
+sudo equo install gcc --ask
+sudo equo i sys-kernel/sabayon-sources:4.19 --ask
+echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
+git clone https://github.com/gordboy/rtl8812au.git
+cd rtl8812au
+echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
+make
+echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
+sudo modprobe 8812au
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
+
+            ;;
+	6)
+                        #!/bin/bash
+echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
+sudo equo install gcc --ask
+sudo equo i sys-kernel/sabayon-sources:4.20 --ask
+echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
+git clone https://github.com/gordboy/rtl8812au.git
+cd rtl8812au
+echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
+make
+echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
+sudo modprobe 8812au
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
+
+            ;;
+	    
+esac
+
+
             ;;
         5)
-			cd shellscripts/
-			bash rtl8812au_uninstall.sh
+			#!/bin/bash
+git clone https://github.com/gordboy/rtl8812au
+cd rtl8812au
+make
+sudo make uninstall
+# for arch
+sudo pacman -R rtl8812au-dkms-git
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m Done! \e[0m"
             ;;
 esac
+
