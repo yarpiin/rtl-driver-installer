c027ed18d960187af6fb2a7417766a6332d1e52f
Alexander <37543796+Alexander88207@users.noreply.github.com>
2019-04-04 21:23:34 +0200

    Also the store offline files for reinstalling question

--- Installer.sh
+++ Installer.sh
@@ -570,6 +570,43 @@ echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
 sudo make install
 sudo modprobe 8812au
 echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
+#!/bin/bash
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=2
+BACKTITLE="RTL8812au-Installer 1.1"
+TITLE="Reinstall after kernel update"
+MENU="Should this installer leave files for a reinstallation after a kernel update?:"
+
+OPTIONS=(1 "Yes"
+         2 "No")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+			#!/bin/bash
+cd -
+echo -e "\e[40;38;5;82m Removeing old files \e[30;48;5;82m\e[0m"
+rm -rf rtl8812au
+echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
+git clone https://github.com/gordboy/rtl8812au.git
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m \e[0m"
+
+            ;;
+        2)
+			#!/bin/bash
+cd -
+echo -e "\e[40;38;5;82m Removeing old files \e[30;48;5;82m\e[0m"
+rm -rf rtl8812au
+echo -e "\e[40;38;5;82m Alright :) \e[30;48;5;82m\e[0m"
 
 esac
 
