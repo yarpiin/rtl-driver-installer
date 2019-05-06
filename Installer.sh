28f9f8a6ffd72f2e4f41dfb04af67f4b47ad07c0
Alexander Vereeken <37543796+Alexander88207@users.noreply.github.com>
2019-05-06 18:28:31 +0200

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -70,7 +70,7 @@ sudo eopkg it gcc binutils git make
 HEIGHT=15
 WIDTH=40
 CHOICE_HEIGHT=2
-BACKTITLE="RTL-Driver-Installer 1.2.2"
+BACKTITLE="RTL-Driver-Installer 1.2.1"
 TITLE="Question about Solus Kernel"
 MENU="What kernel do you use?:"
 
@@ -293,7 +293,7 @@ git clone https://github.com/gordboy/rtl8812au.git
 cd rtl8812au
 echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
 make
-echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+echo -e "\e[40;38;5;82m Installing drMerge pull request #13 from Alexander88207/masteriver \e[30;48;5;82m\e[0m"
 sudo make install
 sudo modprobe 8812au
 echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
