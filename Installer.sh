204d652d1556bd9625d0edf5600f09f274fa565e
Alexander Vereeken <37543796+Alexander88207@users.noreply.github.com>
2019-05-11 18:31:10 +0200

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1177,7 +1177,7 @@ echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
 sudo equo install gcc --ask
 sudo equo i sys-kernel/sabayon-sources:4.14 --ask
 echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
-git clone hhttps://github.com/quickreflex/rtl8188eus
+git clone https://github.com/quickreflex/rtl8188eus
 cd rtl8188eus
 echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
 make all
